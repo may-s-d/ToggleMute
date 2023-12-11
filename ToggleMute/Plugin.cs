9b67af4cc8c866fe61ccfc6a85492709ad52b9b6
may-s-d <may.s.dong@gmail.com>
2023-12-11 17:14:00 -0500

    Initial commit

---
+++ ToggleMute/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace ToggleMute
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("ToggleMute");
+        }
+    }
+}
