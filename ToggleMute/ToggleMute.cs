c36a24afb0b2c5fcea70efc5d23328c710357e38
may-s-d <may.s.dong@gmail.com>
2023-12-12 15:04:36 -0500

    Rename Plugin.cs

---
+++ ToggleMute/ToggleMute.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace ToggleMute
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class ToggleMute : BaseUnityPlugin
+    {
+        public static ConfigEntry<Key> configEntry;
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            Logger.LogInfo("Initializing ToggleMute");
+            
+            var id = "ToggleMute";
+            var shortcut = Key.T;
+            var description = "Toggle mute";
+
+            var bind = Config.Bind(
+                    "Bindings",
+                    id,
+                    shortcut,
+                    description
+            );
+
+            configEntry = bind;
+
+            harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            Logger.LogInfo($"Bound toggle mute to {shortcut}");
+            Logger.LogInfo("Initialized ToggleMute");
+        }
+    }
+}
